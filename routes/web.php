d8add6acceef170fe3bbd61487244330f36ded51
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-10-05 03:54:43 +0300

    aya pages

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,19 @@
 |
 */
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\TestimonialController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ServiceController;
 
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/testimonial', [TestimonialController::class, 'index'])->name('testimonial');
 Route::get('/team', [TeamController::class, 'index'])->name('team');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::get('/service', [ServiceController::class, 'index'])->name('service');
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+Route::get('/blog', [BlogController::class, 'index'])->name('blog');
