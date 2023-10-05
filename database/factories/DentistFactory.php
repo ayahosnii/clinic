7084afa0ea5c1a49c1a8c737b25564f37a94adc0
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-10-05 21:04:17 +0300

    register form

---
+++ database/factories/DentistFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dentist;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DentistFactory extends Factory
+{
+    protected $model = Dentist::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
