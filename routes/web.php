fa2e9914c63d50943a3eb38e35c23599208206ed
Magd <magdelzalameh6@gmail.com>
2023-10-05 04:07:45 +0300

    new pages

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,15 @@
 |
 */
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\TestimonialController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PriceController;
 use App\Http\Controllers\ServiceController;
 
 
@@ -28,3 +31,6 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::get('/service', [ServiceController::class, 'index'])->name('service');
 Route::get('/search', [SearchController::class, 'index'])->name('search');
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+Route::get('/price', [PriceController::class, 'index'])->name('price');
+Route::get('/appointment', [AppointmentController::class, 'index'])->name('appointment');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
