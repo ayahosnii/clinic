a4ae6551405fbfc9176a5c30c261f87cdb37c532
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-10-06 15:36:34 +0300

    middleware

--- app/Http/Middleware/RedirectIfFrontAuthenticated.php
+++ app/Http/Middleware/RedirectIfFrontAuthenticated.php
@@ -22,7 +22,7 @@ class RedirectIfFrontAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
+            if (Auth::guard('dentist')->check()) {
                 return redirect(RouteServiceProvider::FRONT_HOME);
             }
         }
