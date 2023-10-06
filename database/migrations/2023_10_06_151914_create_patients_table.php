cf076413072a9f12edd44fd45815691884020948
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-10-06 23:41:58 +0300

    patient

---
+++ database/migrations/2023_10_06_151914_create_patients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone_number');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->boolean('active')->default(false);
+            $table->string('profile_picture')->default('avatar.jpg');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
